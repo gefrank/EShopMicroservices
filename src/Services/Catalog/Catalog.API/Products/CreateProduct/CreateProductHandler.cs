3c38824986be92778f2f8f07c8bf1db4c924d9d2
gefrank <gefrank@gmail.com>
2025-03-20 13:47:42 -0500

    Added Fluent Validation to a pipeline behavior.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -6,8 +6,25 @@ namespace Catalog.API.Products.CreateProduct
         : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
+    /// <summary>
+    /// Validator for the CreateProductCommand. 
+    /// using FluentValidation to ensure that the command has valid data before it is processed.
+    /// This is known as cross-cutting concern validation, as it applies to all commands of this type.
+    /// </summary>
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator() 
+        { 
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required.");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required.");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
 
-    internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
+
+    internal class CreateProductCommandHandler(IDocumentSession session) 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,13 +1,22 @@
+using BuildingBlocks.Behaviors;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     // Register all the handlers in the assembly, a handler is a class that implements IRequestHandler
     // Tells mediatR where to find our commands and queries classes
-    config.RegisterServicesFromAssembly(typeof(Program).Assembly);  
+    config.RegisterServicesFromAssembly(assembly); 
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>)); // Register the validation behavior
 });
+// Register all the validators in the assembly, a validator is a class that implements IValidator<T>
+// scans the project for any validators and registers them with the DI container
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+builder.Services.AddCarter();
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!); 
