d2be7aabbb6d2368dcb4f9af4e6597079a8dd9b9
gefrank <gefrank@gmail.com>
2025-03-30 13:13:04 -0500

    Added Rich-domain models and Domain Events.

--- src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -1,18 +1,27 @@
 ﻿
 namespace Ordering.Domain.Models
 {
-    public class OrderItem: Entity<Guid>
+    public class OrderItem: Entity<OrderItemId>
     {
-        internal OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
-        {          
+        /// <summary>
+        /// This is an example of primative obession, we are swapping this out for Strongly Typed Ids.
+        /// OrderItem(Guid orderId, Guid productId, int quantity, decimal price)
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="productId"></param>
+        /// <param name="quantity"></param>
+        /// <param name="price"></param>
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {        
+            Id = OrderItemId.Of(Guid.NewGuid()); 
             OrderId = orderId;
             ProductId = productId;
             Quantity = quantity;
             Price = price;
         }
 
-        public Guid OrderId { get; private set; } = default!;
-        public Guid ProductId { get; private set; } = default!;
+        public OrderId OrderId { get; private set; } = default!;
+        public ProductId ProductId { get; private set; } = default!;
         public int Quantity { get; private set; } = default!;
         public decimal Price { get; private set; } = default!;
     }
---
+++ src/Services/Ordering/Ordering.Domain/ValueObjects/CustomerId.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Ordering.Domain.ValueObjects
+{
+    public record CustomerId
+    {
+        public Guid Value { get; }
+        private CustomerId(Guid value) => Value = value;
+
+        /// <summary>
+        /// Of ensures that the CustomerId is created with a valid Guid. not empty.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        /// <exception cref="DomainException"></exception>
+        public static CustomerId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty)
+            {
+                throw new DomainException("CustomerId cannot be empty.");
+            }
+
+            return new CustomerId(value);
+        }
+    }
+}
